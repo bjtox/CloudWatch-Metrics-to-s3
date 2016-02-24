a47efe5872aaf2d7c5aa3385aa4766a5d129fa5d
Antonio Bitonti <antonio.bitonti@gmail.com>
2016-02-24 11:19:25 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,72 @@
+Export-CloudWatch-Metrics
+=======
+
+Export-CloudWatch-Metrics è uno scritp che permette di esportare direttamente le metriche di cloudwatch su S3
+
+## Prerequisiti
+
+The Node.js project maintains multiple types of releases:
+
+* Python 3 o maggiore
+* boto3
+* AWS cli configured on machine
+
+## Link Collegati
+
+* **Download Python** [here](https://www.python.org/downloads/)
+* **boto3** documentation [here](https://boto3.readthedocs.org/en/latest/)
+* **Aws Namespaces** documentation [here](http://docs.aws.amazon.com/AmazonCloudWatch/latest/DeveloperGuide/aws-namespaces.html)
+
+### Run
+
+I valori da passare allo scritps sono:
+* name_space  => ovvero quale tipo di metrica di cloudwach vogliamo ottenere
+* instance_id => id dell'istanza da ispezionare
+* metric_name => il nome della metrica da ottenere
+* statistic   => in che modo la statistica deve essere restituita
+* unit        => in che tipo di unità voglio ricevere le informazioni
+* period      => l'intervallo espresso in secondi
+* start_time  => la data di inizio es YYYY-MM-DD hh:mm:ss
+* end_time    => la data di fine es YYYY-MM-DD hh:mm:ss
+* s3_bucket   => il Bucket s3 su salvare le metriche in formato .log
+
+comando da eseguire 
+
+```
+$ python getCWMetrics.py -n 'nameSpace' -i 'istanceId' -m 'metric _name' -s 'statistic' -u 'unit' -p 'period' -S 'YYYY-MM-DD hh:mm:ss' -E 'YYYY-MM-DD hh:mm:ss' -b 'bucketName'
+```
+
+## Name Space
+
+* **Aws Namespaces** documentation [here](http://docs.aws.amazon.com/AmazonCloudWatch/latest/DeveloperGuide/aws-namespaces.html)
+
+	Sono utilizzabili tutti i name space riportati.
+
+	Nel caso si utilizzino delle metriche di Sistema [vedi qui](http://docs.aws.amazon.com/AmazonCloudWatch/latest/DeveloperGuide/mon-scripts.html) potete utilizzare il nameSpace 
+	"System/Linux"
+
+* **Instance ID** 
+	
+	Inserire l'id della macchina da monitorare es: "i-abcdefghi"
+
+* **Metric Name** documentation [here](http://docs.aws.amazon.com/AmazonCloudWatch/latest/DeveloperGuide/CW_Support_For_AWS.html)
+
+* **Statistics** 
+	Utilizzabile uno tra i seguenti valori : 'SampleCount'|'Average'|'Sum'|'Minimum'|'Maximum'
+
+* **Unit** 
+	Utilizzabile uno tra i seguenti valori : 'Seconds'|'Microseconds'|'Milliseconds'|'Bytes'|'Kilobytes'|'Megabytes'|'Gigabytes'|'Terabytes'|'Bits'|'Kilobits'|'Megabits'|'Gigabits'|'Terabits'|'Percent'|'Count'|'Bytes/Second'|'Kilobytes/Second'|'Megabytes/Second'|'Gigabytes/Second'|'Terabytes/Second'|'Bits/Second'|'Kilobits/Second'|'Megabits/Second'|'Gigabits/Second'|'Terabits/Second'|'Count/Second'|'None'
+
+* **Period** 
+	Occorre passare un valore int che esprima l'intervallo (in secondi) da prendere in considerazione es: -p 3600 => intervallo di 1h
+
+* **Start Time & End Time** 
+	Occorre passare stringa con il seguente formato: "YYYY-MM-DD hh:mm:ss"
+	NB Start Time deve essere minore di End Time
+
+* **Bucket Name** 
+	Occorre passare stringa nome del bucket nel quale si vogliono storare le metriche
+
+### Result
+
+Il risultato che andrete ad ottenere è il seguente: nel bucket di s3 che avete designato come ospitante delle metriche, verrà creato un oggetto con il seguente percorso "metrics-logs/20160210-20160220/i-91f55819_CPUUtilization.log". L'oggetto è formato dal periodo monitorato, l'id della macchina ec2 e il nome della metrica che richiedete.
---
+++ getCWMetrics.py
@@ -0,0 +1,103 @@
+import boto3
+from datetime import datetime
+import json
+import sys, getopt
+from optparse import OptionParser
+
+
+
+def prepareDate(date):
+	result = date.replace(' ','-')
+	result = result.replace(':','-')
+	return result.split('-')
+
+def getMetrics(instanceId, name_space, metricName, statistics, unit, startTime, endTime, period):
+	cloud_watch = boto3.client('cloudwatch')
+
+	response = cloud_watch.get_metric_statistics(
+		Namespace=name_space,
+	    MetricName=metricName,
+	    Dimensions=[
+        	{
+            	'Name': 'InstanceId',
+            	'Value': instanceId
+        	},
+    	],
+	    StartTime=startTime,
+	    EndTime=endTime,
+	    Period=period,
+	    Statistics=[
+	        statistics
+	    ],
+	    Unit=unit
+	)
+
+	for i in response["Datapoints"]:
+		Timestamp = str(i["Timestamp"])
+		i["Timestamp"] = Timestamp
+
+	return response
+
+
+def pushToS3(bucket, key, json):
+	s3 = boto3.client('s3')
+	response = s3.put_object(
+		Body=bytes(str(json), encoding="UTF-8"),
+	    Bucket=bucket,
+	    Key=key,
+	)
+	
+def main():
+	usage = "usage: %prog [options] arg"
+	parser = OptionParser()
+	parser.add_option("-n", "--name_space", dest="name_space", help="set the AWS namespace", metavar="NAMESPACE")
+	parser.add_option("-i", "--instance_id", metavar="INSTANCE_ID" , dest="instance_id", help="set the EC2 instance id")
+	parser.add_option("-m", "--metric_name", metavar="METRIC_NAME" , dest="metric_name", help="set the EC2 instance id")
+	parser.add_option("-s", "--statistic", metavar="STATISTIC" , dest="statistic", help="set the name of Statistics")
+	parser.add_option("-u", "--unit", metavar="UNIT" , dest="unit", help="set the unit Value")
+	parser.add_option("-p", "--period", metavar="PERIOD" , dest="period", help="set the period es 300")
+	parser.add_option("-S", "--start_time", metavar="START_TIME" , dest="start_time", help="set the Start Time es: YYYY-MM-DD hh:mm:ss")
+	parser.add_option("-E", "--end_time", metavar="END_TIME" , dest="end_time", help="set the EC2 End Time es: YYYY-MM-DD hh:mm:ss")
+	parser.add_option("-b", "--bucket_name", metavar="BUCKET_NAME" , dest="bucket_name", help="set the s3 Bucket Name")
+
+	(options, args) = parser.parse_args()
+	if not options.name_space:   # if filename is not given
+		parser.error('Namespace not given')
+	if not options.instance_id:   # if filename is not given
+		parser.error('Instance Id not given')
+	if not options.metric_name:   # if filename is not given
+		parser.error('Metric Name not given')		
+	if not options.statistic:   # if filename is not given
+		parser.error('Statistic not given')
+	if not options.unit:   # if filename is not given
+		parser.error('Unit not given')
+	if not options.start_time:   # if filename is not given
+		parser.error('Start Time not given')
+	if not options.end_time:   # if filename is not given
+		parser.error('End Time not given')	
+	if not options.period:   # if filename is not given
+		parser.error('End Time not given')
+	if not options.bucket_name:   # if filename is not given
+		parser.error('BUCKET_NAME not given')
+
+	options.start_time = prepareDate(options.start_time)
+	options.end_time = prepareDate(options.end_time)
+	name_space = options.name_space
+	instance_id = options.instance_id
+	metric_name = options.metric_name
+	statistic = options.statistic
+	unit = options.unit
+	bucket_name = options.bucket_name
+	period = int(options.period)
+	startTime = datetime(int(options.start_time[0]),int(options.start_time[1]),int(options.start_time[2]),int(options.start_time[3]),int(options.start_time[4]),int(options.start_time[5])).isoformat('T')+'Z'
+	endTime = datetime(int(options.end_time[0]),int(options.end_time[1]),int(options.end_time[2]),int(options.end_time[3]),int(options.end_time[4]),int(options.end_time[5])).isoformat('T')+'Z'
+	
+
+
+	response = getMetrics(instance_id,name_space,metric_name,statistic,unit,startTime,endTime,period)
+	pushToS3(bucket_name,'metrics-logs/'+datetime(int(options.start_time[0]),int(options.start_time[1]),int(options.start_time[2])).strftime("%Y%m%d")+'-'+datetime(int(options.end_time[0]),int(options.end_time[1]),int(options.end_time[2])).strftime("%Y%m%d")+'/'+instance_id+'_'+metric_name+'.log', response)		
+	
+
+
+if __name__ == "__main__":
+    main()
